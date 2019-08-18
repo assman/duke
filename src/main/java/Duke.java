80e39fefbe70f884bc4310fdc045db65426f10fa
Varun Parmar <parmar@u.nus.edu>
2019-08-18 22:20:40 +0800

    just a quick hands on

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
 public class Duke {
+    private static final String LOGO = " ____        _        \n"
+                                     + "|  _ \\ _   _| | _____ \n"
+                                     + "| | | | | | | |/ / _ \\\n"
+                                     + "| |_| | |_| |   <  __/\n"
+                                     + "|____/ \\__,_|_|\\_\\___|\n";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello I'm Duke\n" + "What can I do for you?\n" + "____________________________");
+        processUserInput(getInput());
     }
+
+    public static String getInput() {
+        Scanner input = new Scanner(System.in);
+        String inputString = input.nextLine();
+        input.close();
+        return inputString;
+     }
+
+    public static void processUserInput(String userInput) {
+        String output = userInput;
+        if(output == "bye") {
+            respondToUser("Bye. Hope to see you again soon!");
+            return;
+        }
+        else {
+            respondToUser(output);
+            processUserInput(getInput());
+        }
+        
+    }
+
+    public static void respondToUser(String userResponse) {
+        System.out.println("-----------------------\n" + userResponse + "\n-----------------------\n");
+    }
+
 }
+
+
