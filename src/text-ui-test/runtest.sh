bc91e5839dc1f2b5cc44e9ac0491e16e44a5062e
Varun <varunrocks@icloud.com>
2019-09-01 21:44:34 +0800

    Level-8

---
+++ src/text-ui-test/runtest.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# create bin directory if it doesn't exist
+if [ ! -d "../bin" ]
+then
+    mkdir ../bin
+fi
+
+# delete output from previous run
+if [ -e "./ACTUAL.txt" ]
+then
+    rm ACTUAL.txt
+fi
+
+# compile the code into the bin folder, terminates if error occurred
+if ! javac -cp ../src -Xlint:none -d ../bin /home/varun/Documents/CS2113/duke/src/main/java/*.java
+then
+    echo "********** BUILD FAILURE **********"
+    exit 1
+fi
+
+# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ../bin main.java.Duke < /home/varun/Documents/CS2113/duke/src/text-ui-test/input.txt > ACTUAL.txt
+
+# compare the output to the expected output
+diff ACTUAL.txt /home/varun/Documents/CS2113/duke/src/text-ui-test/EXPECTED.txt
+if [ $? -eq 0 ]
+then
+    echo "Test result: PASSED"
+    exit 0
+else
+    echo "Test result: FAILED"
+    exit 1
+fi
